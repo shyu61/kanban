d16a9ec1088376e0a467e1eca202f5182fbd5f1b
shyu61 <ohs6261@gmail.com>
2021-03-21 00:17:09 +0900

    Adapt typescript and create note crud

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { API } from 'aws-amplify';
 import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
+import { GraphQLResult } from '@aws-amplify/api-graphql';
+import { ListNotesQuery, Note } from './API';
+
+const initialFormState = { name: '', description: '' } as Note;
 
 function App() {
+  const [notes, setNotes] = useState<Note[]>([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const fetchNotes = async () => {
+    const apiData = await API.graphql({ query: listNotes }) as GraphQLResult<ListNotesQuery>;
+    setNotes(apiData.data?.listNotes?.items ?? []);
+  }
+
+  const createNote = async () => {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData }});
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  const deleteNote = async ({ id }: Note) => {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>We now have Auth!</p>
-      </header>
+      <h1>My Notes App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value })}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value })}
+        placeholder="Note description"
+        value={formData.description ?? ''}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
+            </div>
+          ))
+        }
+      </div>
       <AmplifySignOut />
     </div>
   );
